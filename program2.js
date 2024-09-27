2427ed71f0ed51f5b38d2f828c819097723bac69
Shreyank Agrawal <122221017+Shreyank108@users.noreply.github.com>
2024-09-27 12:09:58 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -4,6 +4,8 @@ const decodeTheRing = function (s, p) {
     const m = s.length;
     const n = p.length;
 
+    const dp = Array(n + 1).fill(false).map(() => Array(m + 1).fill(false));
+
   };
   
   module.exports = decodeTheRing;
