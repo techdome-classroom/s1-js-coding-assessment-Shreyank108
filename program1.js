2be8b9cafa8d3317e789972ff4116f911d8703c9
Shreyank Agrawal <122221017+Shreyank108@users.noreply.github.com>
2024-09-27 12:02:06 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -2,7 +2,12 @@ const getTotalIsles = function (grid) {
 
 
   // write your code here 
-  if (!grid || grid.length===0)
+  if (!grid || grid.length===0) return 0 ; 
+
+  const rows = grid.length; 
+  const cols = grid[0].length;  
+
+  
 
 };
 
